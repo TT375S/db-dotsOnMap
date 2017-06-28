a755bc5383b5871c4ffcfada73d1135f44dbafb4
TT375S <tomataka375@gmail.com>
2017-06-28 16:57:42 +0900

    Add POST method. but it won't work.

--- app/controllers/dots_controller.rb
+++ app/controllers/dots_controller.rb
@@ -1,4 +1,5 @@
 class DotsController < ApplicationController
+  protect_from_forgery :except => [:post]
   def index
   	@dots = Dot.all
   end
@@ -7,6 +8,23 @@ class DotsController < ApplicationController
   end
 
   def searchRectangle
+	@dots = Dot.all
+	render :json => @dots
+  end
+
+  def post
+
+	hashed_json = JSON.parse((request.body.read))
+	hashed_json.each do |res|
+		dot = Dot.new
+		dot.typename = res["typename"]
+		dot.latitude = res["latitude"]
+		dot.longtitude = res["longtitude"]
+		dot.speed = res["speed"]
+		dot.degree = res["degree"]
+		dot.time = res["time"]
+		dot.save
+	end
 	@dots = Dot.all
 	render :json => @dots
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get 'dots/api/search/rectangle/json', to: 'dots#searchRectangle'
 
+  post 'dots/api/post/json', to: 'dots#post'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
